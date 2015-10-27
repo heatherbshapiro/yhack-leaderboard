22fdc53e4478c4e425332e019adb0deb19c3f5b7
Heather Shapiro <hshapiro@microsoft.com>
2015-10-27 11:11:30 -0400

    added leaderboard

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var WindowsAzure= require('azure');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+var config = require('./config');
+var https = require('https');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -58,3 +63,60 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+// require('cylon').robot({
+//     work: function (my) {
+//         //send messages every 4 seconds...
+//         every((4).second(), function () {
+//             postData();
+//         });
+//     }
+// }).start();
+// function postData() {
+
+//     var user = {
+//         firstName: 'Heather',
+//         lastName: 'Shapiro',
+//         score: 30
+//     };
+
+//     var userString = JSON.stringify(user);
+
+//     var headers = {
+//         'Accept': "application/json",
+//         'Content-Type': 'application/json',
+//         'Content-Length': userString.length,
+//         'X-ZUMO-APPLICATION': config.appkey
+//     };
+
+//     var options = {
+//         host: config.mobileservices + '.azure-mobile.net',
+//         port: 443,
+//         path: '/tables/' + config.table,
+//         method: 'POST',
+//         headers: headers
+//     };
+
+//     // Setup the request.  The options parameter is
+//     // the object we defined above.
+//     var req = https.request(options, function (res) {
+//         res.setEncoding('utf-8');
+
+//         var responseString = '';
+
+//         res.on('data', function (data) {
+//             responseString += data;
+//         });
+
+//         res.on('end', function () {
+//             var resultObject = JSON.parse(responseString);
+//         });
+//     });
+
+//     req.on('error', function (e) {
+//         // TODO: handle error.
+//     });
+
+//     req.write(userString);
+//     req.end();
+// }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var sort = require('sort')
 
-/* GET home page. */
+var url = "https://leaderboarddance.azure-mobile.net/tables/scores";
+
+// var dat = request(url, function(error,response,body){
+//   console.log(JSON.parse(body));
+// })
+
+var sortColumnName = "score";
+
+function SortByName(x,y) {
+      return ((x[sortColumnName]  == y[sortColumnName]) ? 0 : ((x[sortColumnName]< y[sortColumnName]) ? 1 : -1 ));
+    }
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Meow' });
+  request(url,function(error,response,body){
+    res.render('index', { title: 'YHack Dancing LeaderBoard', data:JSON.parse(body).sort(SortByName).slice(1,11)});
+  });
 });
 
 module.exports = router;
